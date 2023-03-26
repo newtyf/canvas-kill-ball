302a90979860a1fe00a7ffa1903e21a6ef26d3b1
newt <axelskam.123@outlook.es>
2023-03-25 22:48:01 -0500

    init project game

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// document.body.style.overflow = 'hidden';
